7f24185a2b72a5b4e7a675b9c7e3901f3ba7c458
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-05-24 15:57:04 -0600

    changed db path

--- tendril_exercise/settings.py
+++ tendril_exercise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_)7^b%7-itvjfr47s(_%us+d4*7utpd)c6#e+d=vkw-o9ds!ug'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'songs',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'tendril_exercise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'tendril_exercise.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'subset_track_metadata.db'),
     }
 }
 
