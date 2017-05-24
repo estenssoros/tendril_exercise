981c7b714802f6829f5e8b5d49fafca44a1cdd54
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-05-24 15:57:33 -0600

    updated

--- songs/scripts/part1.py
+++ songs/scripts/part1.py
@@ -4,18 +4,30 @@ import os
 import sqlite3
 import sys
 import time
+from argparse import ArgumentParser
 
 import pandas as pd
 from django.conf import settings
 from tqdm import tqdm
 
 import common_env
+import mylogger
+
+parser = ArgumentParser(description='validate sqoop commands by comparing mysql to hadoop')
+parser.add_argument('--run', action="store_true")
+parser.add_argument('--test', action="store_true")
+args = parser.parse_args()
 
 
 class Songs(object):
+    '''
+    Connects to sqlite3 database and create sha256 column
+    '''
+
     def __init__(self, db_path):
         self.conn = sqlite3.connect(db_path)
         self.curs = self.conn.cursor()
+        self.logger = mylogger.mylogger()
         self.df = self.get_df()
 
     def hasher(self, s):
@@ -23,11 +35,14 @@ class Songs(object):
         return hash_obj.hexdigest()
 
     def get_df(self):
-        df = pd.read_sql('select * from songs', con=self.conn)
+        self.logger.info('reading data from sqlite3')
+        df = pd.read_sql('select track_id, artist_name from songs', con=self.conn)
+        self.logger.info('converting artist name to sha256')
         df['sha256'] = df.apply(lambda x: self.hasher(x['artist_name']), axis=1)
         return df
 
     def update_table(self):
+        self.logger.info('updating table')
         # 1.44s user 0.14s system 99% cpu 1.596 total
         sql = '''UPDATE songs SET sha256 = ? WHERE track_id = ?'''
         count = 1
@@ -45,13 +60,15 @@ class Songs(object):
 
     def run(self):
         self.update_table()
+        self.shutdown()
 
 
 def main():
-    db_path = os.path.join(settings.BASE_DIR,'subset_track_metadata.db')
-    songs = Songs(db_path)
-    songs.run()
-    songs.shutdown()
+    songs = Songs(settings.DATABASES['default']['NAME'])
+    if args.run:
+        songs.run()
+    elif args.test:
+        pass
 
 
 if __name__ == '__main__':
