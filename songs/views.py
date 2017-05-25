5e62f89c7e321177e207b336337723943c63cbd6
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-05-24 20:36:55 -0600

    began creating result views

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from scripts.plotting import familiarity_v_hotness, hotness_v_duration
+
+
+def home(request):
+    return render(request, 'songs/home.html')
+
+
+def results(request, chart_type=None):
+    plotters = {'familiarity_v_hotness': familiarity_v_hotness,
+                'hotness_v_duration': hotness_v_duration}
+    if chart_type == None:
+        plotter = familiarity_v_hotness
+    else:
+        plotter = plotters[chart_type]
+
+    script, div = plotter()
+    context = {'title': chart_type, "the_script": script, "the_div": div}
+    return render(request, 'songs/results.html', context)
