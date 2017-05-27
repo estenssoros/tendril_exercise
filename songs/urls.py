d65e759496ed75c8a422f0a3a3c4bcdfffeccd1e
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-05-26 21:22:11 -0600

    added api endpoints

--- songs/urls.py
+++ songs/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^results/$', views.results),
     url(r'^api/chart/$', views.ChartAPI.as_view()),
     url(r'^api/plot_api/$', views.PlotAPI.as_view()),
+    url(r'^api/artist_autocomplete/$', views.ArtistAutoCompleteAPI.as_view()),
+    url(r'^api/song_autocomplete/$', views.SongAutoCompleteAPI.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
